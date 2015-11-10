69e1f1be47ab2fa3e03969955cf0476f057dcd5d
Hans-Filip Elo <hansfilipelo@gmail.com>
2015-11-10 10:14:15 +0100

    Now deletes views as well as tables.

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -1,13 +1,15 @@
 
 /* Initiate script with "clean" DB */
-SET FOREIGN_KEY_CHECKS = 0; 
-SET @tables = NULL;
-SELECT GROUP_CONCAT(table_schema, '.', table_name) INTO @tables
-  FROM information_schema.tables 
-  WHERE table_schema = 'tddd37'; -- specify DB name here.
+SET FOREIGN_KEY_CHECKS = 0;
+SET GROUP_CONCAT_MAX_LEN=32768;
+SET @views = NULL;
+SELECT GROUP_CONCAT('`', TABLE_NAME, '`') INTO @views
+  FROM information_schema.views
+  WHERE table_schema = (SELECT DATABASE());
+SELECT IFNULL(@views,'dummy') INTO @views;
 
-SET @tables = CONCAT('DROP TABLE ', @tables);
-PREPARE stmt FROM @tables;
+SET @views = CONCAT('DROP VIEW IF EXISTS ', @views);
+PREPARE stmt FROM @views;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 SET FOREIGN_KEY_CHECKS = 1;
