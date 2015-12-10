910745003dafbc0deb5bf2a43aac7dfe13396ef8
Hans-Filip Elo <hansfilipelo@gmail.com>
2015-12-10 16:57:30 +0100

    Where we stop working on thursday

--- lab4/stored_procedures.sql
+++ lab4/stored_procedures.sql
@@ -1,3 +1,5 @@
+/* Our stored procedures  */
+DROP PROCEDURE IF EXISTS addYear;
 SELECT 'Add procedure addYear for inserting new year' AS '';
 delimiter //
 CREATE PROCEDURE addYear (IN year INT, IN factor DOUBLE)
@@ -6,25 +8,32 @@ INSERT INTO Year (year, profitfactor) VALUES ( year, factor );
 END//
 delimiter ;
 
+DROP PROCEDURE IF EXISTS addDay;
 SELECT 'Add procedure addDay for inserting new weekday into schedule' AS '';
 delimiter //
 CREATE PROCEDURE addDay (IN year INT, IN day VARCHAR(45), IN factor DOUBLE) BEGIN INSERT INTO Weekday (name, weekdayFactor, year) VALUES (day, factor, (SELECT idYear FROM Year WHERE Year=year)); END//
 delimiter ;
 
+DROP PROCEDURE IF EXISTS addDestination;
 SELECT 'Add procedure addDestination for inserting new destination' AS '';
 delimiter //
-CREATE PROCEDURE addDestination(IN airport_code VARCHAR(3), IN name VARCHAR(45), IN country VARCHAR(45)) 
-BEGIN 
-INSERT INTO Destination (airportId, name, country) VALUES (airport_code, name, country); 
+CREATE PROCEDURE addDestination(IN airport_code VARCHAR(3), IN destName VARCHAR(45), IN country VARCHAR(45))
+BEGIN
+INSERT INTO Country (name) VALUES (UPPER(country)) ON DUPLICATE KEY UPDATE name=UPPER(country);
+INSERT INTO Destination (airportId, name, country) VALUES (airport_code, destName, (select idCountry from Country where name=UPPER(country)));
 END//
 delimiter ;
 
-
+DROP PROCEDURE IF EXISTS addRoute;
 SELECT 'Add procedure addDestination for inserting new destination' AS '';
 delimiter //
-CREATE PROCEDURE addRoute(IN departure_airport_code VARCHAR(3), IN arrival_airport_code VARCHAR(3), IN year INT(11), IN routeprice INT(11)) 
-BEGIN 
-INSERT INTO Route (from, to, routePrice, year) VALUES ((select airportId from Destination where airportId=departure_airport_code), (select airportId from Destination where airportId=arrival_airport_code), routeprice, year); 
+CREATE PROCEDURE addRoute(IN departureAirportCode VARCHAR(3), IN arrivalAirportCode VARCHAR(3), IN inYear INT(11), IN inRoutePrice INT(11))
+BEGIN
+INSERT INTO Route (from, to, routePrice, year)
+VALUES (
+  (select airportId from Destination where airportId=departureAirportCode),
+  (select airportId from Destination where airportId=arrivalAirportCode),
+  inRoutePrice,
+  (select idYear from Year where year=inYear));
 END//
 delimiter ;
-
